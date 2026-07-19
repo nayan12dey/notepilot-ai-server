1ef602b4025b2b658837514f15ee5a5f40f83564
Nayan Dey <ndeynayan123@gmail.com>
2026-07-20 05:27:03 +0530

    feat: add API endpoint for creating notes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require("cors")
 const dotenv = require('dotenv')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
 
 
 const app = express()
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT
 
 const client = new MongoClient(uri, {
@@ -22,6 +25,42 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const db = client.db("notepilot-ai");
+
+        const usersCollection = db.collection("users");
+        const notesCollection = db.collection("notes");
+
+        // add notes
+        app.post("/notes", async (req, res) => {
+            try {
+                const noteData = req.body;
+
+                const newNote = {
+                    ...noteData,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                };
+
+
+                const result = await notesCollection.insertOne(newNote);
+
+
+                res.status(201).json({
+                    success: true,
+                    message: "Note added successfully",
+                    noteId: result.insertedId
+                });
+
+
+            } catch (error) {
+                console.log(error);
+
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to add note"
+                })
+            }
+        });
 
 
 
@@ -34,7 +73,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
